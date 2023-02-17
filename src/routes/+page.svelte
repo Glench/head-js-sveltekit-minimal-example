47b89ee774bc73902f96ba0596c61c63c46a018d
Glen Chiacchieri <www@glench.com>
2023-02-17 14:40:24 -0500

    upload

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import {onMount} from 'svelte';
+
+    let message = '...'
+    onMount(() => {
+        if (Stripe) {
+           message = 'Stripe() is available!' 
+        }
+    })
+</script>
+<svelte:head>
+    <script src="https://js.stripe.com/v3/"></script>
+</svelte:head>
+<h1>{message}</h1>
+
+<p>Above message should always be "Stripe() is available!"</p>
+
+<a href="/other">other</a>
